5d97a585370d09fb45099220117bc62f0ea81393
Joel Margosian <margosij@gmail.com>
2020-01-18 10:21:53 -0500

    hopefully fixing build bug

--- client/src/components/SaveButton/index.js
+++ client/src/components/SaveButton/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function SaveBtn(props){
     return(
-        <>
+        <div>
         <button type="button" 
         onClick={e => props.onClick(e)} 
         className={`btn btn-success mx-2 ${props["data-value"]}`} {...props} >
             Save
         </button>
-        </>
+        </div>
     )
 }
 
